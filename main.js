8a2452818d92814bd642e8c795b07795a725a7a4
drgugger <User@Macs-MacBook-Pro.local>
2016-12-04 21:16:18 -0500

    Latest Code

---
+++ main.js
@@ -0,0 +1,9 @@
+var age = 49;
+
+if (age < 12) {
+   console.log("You're a child.");
+} else if (age < 20) {
+   console.log("You're a teenager.");
+} else {
+   console.log("You're probably an adult.");
+}
